130b44e9eeb28fa07f7a414fbef804f1ff811127
Pavel Appolonov <app.pavel1998@gmail.com>
2025-06-08 19:28:13 +0300

    fix feign client

--- core/interaction-api/src/main/java/ru/practicum/event_service/feign/EventClient.java
+++ core/interaction-api/src/main/java/ru/practicum/event_service/feign/EventClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.practicum.event_service.dto.EventFullDto;
 
-@FeignClient(name = "main-service", path = "/events")
+@FeignClient(name = "event-service", path = "/events")
 public interface EventClient {
 
     @GetMapping("/by-id/{id}")
