3daab46c5f0b975422c2b3c1eac16e8b8d703eca
Pavel Appolonov <app.pavel1998@gmail.com>
2025-06-12 14:52:49 +0300

    Collector service added

---
+++ stats/collector/src/main/java/ru/practicum/kafka/KafkaCollectorProducer.java
@@ -0,0 +1,32 @@
+package ru.practicum.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.KafkaException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaCollectorProducer {
+    private final KafkaProducer<String, SpecificRecordBase> kafkaProducer;
+
+    public void send(String topic, SpecificRecordBase data) {
+        try {
+            kafkaProducer.send(new ProducerRecord<>(topic, data),
+                    (metadata, e) -> {
+                        if (e != null) {
+                            log.error("[{}] Ошибка отправки: {}", topic, e.getMessage());
+                        } else {
+                            log.info("Отправлено в {} - {}", topic, metadata.partition());
+                        }
+                    });
+        } catch (KafkaException ex) {
+            log.error("Ошибка при отправлении сообщения:", ex);
+            throw new KafkaException("Ошибка при отправлении сообщения", ex);
+        }
+    }
+}
