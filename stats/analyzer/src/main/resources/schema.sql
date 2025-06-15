7247194343ea48a386d44f79223c9ed8ed30356c
Pavel Appolonov <app.pavel1998@gmail.com>
2025-06-15 12:51:20 +0300

    q fix analyzer yaml

--- stats/analyzer/src/main/resources/schema.sql
+++ stats/analyzer/src/main/resources/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE event_similarity (
     event_b BIGINT NOT NULL,
     score DOUBLE PRECISION NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY (event_a, event_b),
-    CONSTRAINT ordered_ids CHECK (event_a < event_b)
+    PRIMARY KEY (event_a, event_b)
+--    CONSTRAINT ordered_ids CHECK (event_a < event_b)
 );
 
 CREATE TABLE user_action (
