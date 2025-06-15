b5017f351c2de75170412d0804a34f7d54e1c7f7
Pavel Appolonov <app.pavel1998@gmail.com>
2025-06-15 12:56:58 +0300

    q fix analyzer yaml

--- stats/analyzer/src/main/resources/schema.sql
+++ stats/analyzer/src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS event_similarity;
 DROP TABLE IF EXISTS user_action;
 
-CREATE TABLE event_similarity (
+CREATE TABLE IF NOT EXISTS event_similarity (
     event_a BIGINT NOT NULL,
     event_b BIGINT NOT NULL,
     score DOUBLE PRECISION NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY (event_a, event_b)
-
+    PRIMARY KEY (event_a, event_b),
+    CONSTRAINT ordered_ids CHECK (event_a < event_b)
 );
 
-CREATE TABLE user_action (
+CREATE TABLE IF NOT EXISTS user_action (
     user_id BIGINT NOT NULL,
     event_id BIGINT NOT NULL,
     action_type VARCHAR(20) NOT NULL,
